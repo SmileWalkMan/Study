94df8723d169c50dcf4b625af5c032438b746db3
Bob <smilewalkman@sina.com>
2021-06-25 16:28:38 +0800

    全部进退位

--- Study/src/main/java/com/jj/math/Onehundred.java
+++ Study/src/main/java/com/jj/math/Onehundred.java
@@ -9,6 +9,7 @@ import java.util.Random;
 public class Onehundred {
 
 	public static String one() {
+		
 		Random random = new Random();
 		int a = random.nextInt(90)+11;
 		int b = random.nextInt(a)+10;
@@ -19,6 +20,15 @@ public class Onehundred {
 		}
 		int zz=random.nextInt(2);
 		if(zz==0) {
+			while(!isJinwei(b,c)) {
+				a = random.nextInt(90)+11;
+				b = random.nextInt(a)+10;
+				c=a-b;
+				while(c<=0) {
+					b = random.nextInt(a)+10;
+					c=a-b;
+				}
+			}
 			String B = b+"";
 			if(b<10) B+=" ";
 			String C = c+"";
@@ -28,6 +38,15 @@ public class Onehundred {
 			else
 				return C+"+"+B+"=";
 		}else {
+			while(!isTuiwei(a,b)) {
+				a = random.nextInt(90)+11;
+				b = random.nextInt(a)+10;
+				c=a-b;
+				while(c<=0) {
+					b = random.nextInt(a)+10;
+					c=a-b;
+				}
+			}
 			String B = b+"";
 			if(b<10) B+=" ";
 			String A = a+"";
@@ -35,9 +54,27 @@ public class Onehundred {
 			return A+"-"+B+"=";
 		}
 	}
+	public static boolean isJinwei(int a,int b) {
+		int c = (a+b)/10;
+		int cc= a/10 + b/10;
+		if(c>cc) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+	public static boolean isTuiwei(int a,int b) {
+		int c = (a-b)/10;
+		int cc= a/10 - b/10;
+		if(c < cc) {
+			return true;
+		}else {
+			return false;
+		}
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		for(int i=1;i<=2000;i++) {
+		for(int i=1;i<=1000;i++) {
 			System.out.print(one());
 			if(i%4==0)System.out.println();
 			else System.out.print("        ");
